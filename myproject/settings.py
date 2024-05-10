d2a9e6c43888e68cd3c644a0d493540ecb29aeee
AswinSBalakrishnan <aswinsbalakrishnan@gmail.com>
2024-05-10 22:03:05 +0530

    aws push

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4nkfyd27houe11^jjs_6s0i@a8nhk*0)$=k8**e+7cu4wpss@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
